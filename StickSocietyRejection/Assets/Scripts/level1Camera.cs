22c9165580c547ec663c9786299282314f6d8782
Joy Hauser <Joy Hauser>
2017-02-11 12:11:49 -0600

    Have camera move based on time

---
+++ StickSocietyRejection/Assets/Scripts/level1Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level1Camera : MonoBehaviour {
+    int currentTick;
+    static int rate = 5; // This is seconds per ft movement
+
+	// Use this for initialization
+	void Start () {
+        currentTick = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTick++;
+
+		if ((currentTick % 5) == 0) {
+            transform.Translate(new Vector3(rate * Time.deltaTime, 0, 0));
+        }
+	}
+}
